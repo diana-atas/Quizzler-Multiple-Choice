4bfd133973247f84025cb2ddf1f0fde95cfd43f8
Diana Atas <diana.atas@trademe.co.nz>
2022-12-26 17:27:22 +1300

    complete layout practice

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var answerA: UIButton!
+    @IBOutlet weak var answerB: UIButton!
+    @IBOutlet weak var answerC: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func answerPressed(_ sender: Any) {
+    }
+    
 
 }
 
