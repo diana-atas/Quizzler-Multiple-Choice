50a442c1e47d1205f1773b7054fb15db8bbb42d5
Diana Atas <diana.atas@trademe.co.nz>
2022-12-26 22:46:03 +1300

    display question on screen

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+
+// DA psuedo-code
+//  replace Question Text with actual question
+//  check if user's answer is correct
+//  go to the next question
+//  add score
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,14 +20,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerA: UIButton!
     @IBOutlet weak var answerB: UIButton!
     @IBOutlet weak var answerC: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
     
+    var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        questionLabel.text = quizBrain.getQuestion()
     }
 
-    @IBAction func answerPressed(_ sender: Any) {
+    @IBAction func answerPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        print("userAnswer: \(userAnswer)")
     }
     
 
